d428d33c1830fcb642904037e129a5d8b5378f1a
antbrothers <jianxi_lin@163.com>
2018-03-18 23:11:20 +0800

    修改配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
-var webpack = require('webpack')
-var path = require('path')
-var loaders = require('./webpack.loaders')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: [
-    
-  ]
+const commonConfig = require('./webpack.common.config.js')
+
+const publicConfig = {
+  devtool: 'cheap-module-source-map',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: ["css-loader?modules&localIdentName=[local]-[hash:base64:5]", "postcss-loader"]
+            })
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist/*.*']),
+        new UglifyJSPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new ExtractTextPlugin({
+            filename: '[name].[contenthash:5].css',
+            allChunks: true
+        })
+    ]
 }
+
+module.exports = merge(commonConfig, publicConfig)
