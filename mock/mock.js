9ce6cd5f1ab8618c5c8397ecf90e76ba7de3e688
Administrator <jianxi_lin@163.com>
2018-03-20 15:29:15 +0800

    添加node服务

---
+++ mock/mock.js
@@ -0,0 +1,10 @@
+let Mock = require('mockjs')
+var Random = Mock.Random
+module.exports = function() {
+    var data = {}
+    data.user = {
+        'name': Random.cname(),
+        'intro': Random.word(20)
+    }
+    return data
+}
