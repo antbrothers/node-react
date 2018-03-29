2431f05075d96a586f4361ec0f0ed3fa2a6eecda
Administrator <jianxi_lin@163.com>
2018-03-29 17:48:06 +0800

    mocha api post 测试

--- server/config/error.message.js
+++ server/config/error.message.js
@@ -1,5 +1,4 @@
-const tip = {
-    
+const tip = {    
     LOGIN_SUCCESS: "登录成功"
 }
 module.exports = tip;
--- server/test/test.js
+++ server/test/test.js
@@ -4,17 +4,26 @@ const request = require('supertest'),
 describe('#test express app', () => {
   let server = app.listen(9900);
   describe('#test server', () => {
-    it('#test GET /api/products', async () => {
-      // let res = await request(server)
-      //     .get('/api/products')
-      //     .expect('Content-Type', 'application/json; charset=utf-8')
-      //.expect(200, '{"code":200,"message":"请求成功","data":[{"id":"f9f3ec07-4711-46b2-a223-5fb562ca72b0","userName":"linjianxi","firstName":"linjianxi","lastName":"linjianxi","passWord":"14782543977","status":"active","createdAt":"2018-03-27T01:43:57.000Z","updatedAt":"2018-03-27T01:43:57.000Z"}]}');
-
+    it('#test GET /api/products', async () => {    
       let res = await request(server)
         .get('/api/products')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        .expect(200, '接口测试查看返回结果');
+        //.expect(200, '接口测试查看返回结果');
+    })
+
+    it ('#test Post /api/insertUser', async () => {
+      let result = await request(server)
+        .post('/api/insertUser') 
+        .send({
+          userName: 'l',
+          firstName: '2',
+          lastName: '2',
+          passWord: '5',
+          status: '1'
+        })
+        //.expect(200, 'post 提交api测试')
+        .expect(200)
     })
   })
 })
