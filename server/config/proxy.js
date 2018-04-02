f3480198bc22f632ab18bff7075bd65e02aa19e1
Administrator <jianxi_lin@163.com>
2018-04-02 17:55:35 +0800

    添加一个测试模块

--- server/config/proxy.js
+++ server/config/proxy.js
@@ -2,13 +2,13 @@
  * @Author: jianxi_lin 
  * @Date: 2018-04-02 14:40:46 
  * @Last Modified by: jianxi_lin
- * @Last Modified time: 2018-04-02 15:00:02
+ * @Last Modified time: 2018-04-02 17:34:17
  */
 
  module.exports = {
      proxyTable: {
          '/external': {
-             target: 'http://www.123369.com.cn/api/ServiceCommunityIdList?communityID=0&city=0',
+             target: 'https://activity.waimai.meituan.com/coupon/grabShareCoupon',
              changeOrigin: true,
              pathRewrite: {
                  '^/external': '/'
